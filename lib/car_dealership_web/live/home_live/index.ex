9f59a3ad144199bd3e2fffbd86c50845b38d0578
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-10-10 15:36:37 +0300

    added queries

--- lib/car_dealership_web/live/home_live/index.ex
+++ lib/car_dealership_web/live/home_live/index.ex
@@ -3,22 +3,67 @@ defmodule CarDealershipWeb.HomeLive.Index do
   alias CarDealership.Cars
   alias CarDealership.Cars.Car
   use CarDealershipWeb, :live_view
+  alias CarDealership.Models
 
   def handle_event("validate", params, socket) do
-    category = Categories.list_categories()
-    {:noreply,socket}
+
+
+    IO.inspect params
+
+
+    models = Categories.get_category!(String.to_integer(params["car"]["type"])).models
+    |> Enum.map(fn x -> {x.name, x.id} end)
+
+    cars = if params["car"]["brand"] != nil do
+      Models.get_model!(String.to_integer(params["car"]["brand"])).cars
+    else
+      []
+    end
+    # cars = Models.get_model!(String.to_integer(params["car"]["brand"])).cars
+    IO.inspect cars
+
+    year = if params["car"]["year"] != nil do
+      params["car"]["year"]
+    else
+      ""
+    end
+
+    minimum_price = if params["car"]["min"] != nil do
+      params["car"]["minimum_price"]
+    else
+      ""
+    end
+
+    maximum_price = if params["car"]["max"] != nil do
+      params["car"]["maximum_price"]
+    else
+      ""
+    end
+
+    # Enum.filter(cars, fn x -> x.year == year end)
+    # |> Enum.filter(fn x -> x.price >= minimum_price end)
+    # |> Enum.filter(fn x -> x.price <= maximum_price end)
+    # |> Enum.map(fn x -> {x.name, x.id} end)
+
+
+    {:noreply,
+    socket
+    |> assign(:models, models)
+    |> assign(:cars, cars)}
   end
 
   @impl true
   def mount(_params, _session, socket) do
     changeset = Cars.change_car(%Car{})
     categories = Categories.list_categories()
-    mapped_categories = Enum.map(categories, fn x -> x.name end)
-    IO.inspect changeset
+
+    |> Enum.map(fn x -> {x.name,x.id} end)
+
     {:ok, socket
     |> assign(:cars, Cars.list_cars())
     |> assign(:changeset, changeset)
-    |> assign(:mapped_categories, mapped_categories)}
+    |> assign(:categories, categories)
+    |> assign(:models, [])}
   end
 
 
